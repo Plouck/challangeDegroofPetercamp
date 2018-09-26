533eda8e10b7b77db222a7a627e3424e4786af65
Antoni Dalle Nogare <antoni.dallenogare@gmail.com>
2018-09-26 12:31:12 +0200

    remise en état

--- ChallengeDegroofPetercam/app/src/main/java/tonidano/challengedegroofpetercam/LoginActivity.java
+++ ChallengeDegroofPetercam/app/src/main/java/tonidano/challengedegroofpetercam/LoginActivity.java
@@ -3,6 +3,7 @@ package tonidano.challengedegroofpetercam;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -93,7 +94,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
-
+    public void personalpage (View view) {
+        startActivity(new Intent(this, personalpageActivity.class));
+    }
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
             return;
@@ -347,4 +350,3 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
 }
-
